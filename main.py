f0af2254dc024a656365f407eaf439532adae383
Faveing2 <MPearsonpear@protonmail.com>
2020-03-04 19:59:19 -0800

    fix

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import sys
 from lib.waveshare_epd import epd2in7
 import time
 
-try:
-    epd = epd2in7.EPD()
+epd = epd2in7.EPD()
 
-    epd.init()
-    epd.Clear(0xFF)
+epd.init()
+epd.Clear(0xFF)
 
-    draw.text((10, 0), 'hello world', font = font24, fill = 0)
+draw.text((10, 0), 'hello world', font = font24, fill = 0)
 
-    epd.display(epd.getbuffer(Himage))
+epd.display(epd.getbuffer(Himage))
 
-    epd.Clear(0xFF)
-
-except:
-    print("error")
+epd.Clear(0xFF)
