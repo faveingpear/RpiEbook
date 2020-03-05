8b110cb421bdd40cd327f470932b53bd70839b48
Faveing2 <MPearsonpear@protonmail.com>
2020-03-04 20:50:17 -0800

    Working on text format

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from lib.waveshare_epd import epd2in7
 from PIL import Image,ImageDraw,ImageFont
 import time
 
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
 class inkdisplay():
 
     fontsdir = ""
@@ -55,12 +59,61 @@ class inkdisplay():
 
         self.wrapper = textwrap.TextWrapper(width=45)
 
+class page():
+
+    # TODO Define this more!
+
+    text = ""
+    pageNumber = ""
+    
+    def getText(self):
+        return self.text
+
+    def __init__(self, newText, newPageNumber):
+        self.text = newText
+        self.pageNumber = newPageNumber
+
 d = inkdisplay(epd2in7.EPD(), "fonts", "Ubuntu-R.ttf")
 
-d.newScreen()
-d.addText("This is a test of a very long string of text to see if the display will loop the text or just clip the end of this line.", 10, 0)
-d.drawScreen()
-d.clear()
+pages = {}
+
+for i in range(2):
+    pages[i] = page("This is a test of a very long string of text to see if the display will loop the text or just clip the end of this line." + i, i)
+
+pageNumber = 0
+
+key1 = 5
+key2 = 6
+key3 = 13
+key4 = 19
+
+GPIO.setup(key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(key4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+        key1state = GPIO.input(key1)
+        key2state = GPIO.input(key2)
+        key3state = GPIO.input(key3)
+        key4state = GPIO.input(key4)
+
+        if key1state == False:
+            page = page + 1
+            d.clear()
+            d.newScreen()
+            d.addText(pages[pageNumber].getText(), 10, 0)
+            d.drawScreen()
+            time.sleep(0.2)
+        if key2state == False:
+            d.clear()
+            time.sleep(0.2)
+        if key3state == False:
+            d.clear()
+            time.sleep(0.2)
+        if key4state == False:
+            d.clear()
+            time.sleep(0.2)
 
 # epd = epd2in7.EPD()
 
