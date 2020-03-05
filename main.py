782614adf45e1fff8a6244fb54026e57ccfccd90
Faveing2 <MPearsonpear@protonmail.com>
2020-03-04 20:05:59 -0800

    add font

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from lib.waveshare_epd import epd2in7
 from PIL import Image,ImageDraw,ImageFont
@@ -9,10 +10,14 @@ epd = epd2in7.EPD()
 epd.init()
 epd.Clear(0xFF)
 
+fontsdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fonts')
+
+font24 = ImageFont.truetype(os.path.join(fontsdir, 'Ubuntu-R.ttc'), 24)
+
 Himage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
 draw = ImageDraw.Draw(Himage)
 
-draw.text((10, 0), 'hello world', fill = 0)
+draw.text((10, 0), 'hello world', font = font24, fill = 0)
 
 epd.display(epd.getbuffer(Himage))
 
