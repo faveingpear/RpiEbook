0756e024ffebf00e68c964b56707b0b6847e0c3f
Faveing2 <MPearsonpear@protonmail.com>
2020-03-04 20:01:50 -0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from lib.waveshare_epd import epd2in7
+from PIL import Image,ImageDraw,ImageFont
 import time
 
 epd = epd2in7.EPD()
@@ -8,7 +9,10 @@ epd = epd2in7.EPD()
 epd.init()
 epd.Clear(0xFF)
 
-draw.text((10, 0), 'hello world', font = font24, fill = 0)
+Himage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+draw = ImageDraw.Draw(Himage)
+
+draw.text((10, 0), 'hello world', fill = 0)
 
 epd.display(epd.getbuffer(Himage))
 
